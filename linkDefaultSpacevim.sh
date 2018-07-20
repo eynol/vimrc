d0798962bddea53486ae9e0745b290e6b4d418ba
eynol <625608852@qq.com>
2018-07-20 17:46:20 +0800

    tar old .SpaceVim.d to .tar file

--- linkDefaultSpacevim.sh
+++ linkDefaultSpacevim.sh
@@ -1,27 +1,44 @@
 #!/bin/sh
-stamp=`date +%Y%m%d_%H%M%S`
-if ! [ -d $HOME/.SpaceVim.d ] ; then
-  echo "[Failed]It seems that you did't install SpaceVim."
+
+if ! [ -d $HOME/.SpaceVim.d ]
+then
+  echo "[Failed]It seems that you did't install SpaceVim. Opration aborted."
   exit 1
-else
-  # check if the folder has been linked
-  if [ $HOME/.SpaceVim.d -ef `pwd`/.SpaceVim.d ];then
-    echo "[Succeeded]Already linked, do nothing."
-    exit 0
-  fi
-  echo "Backup old SpaceVim.d"
-  if ! (( mv $HOME/.SpaceVim.d $HOME/.SpaceVim.d.$stamp )) ; then
-    echo "[Failed]Backup failed."
-    exit 1
-  else
-    echo "Ln -s ....."
-    if ! (( ln -s `pwd`/.SpaceVim.d $HOME/.SpaceVim.d )) ; then
-      echo "[Failed]Link folder failed."
-      exit 1
-    else
-      echo "[Succeeded!]"
-    fi
-  fi
 fi
 
+# check if the folder has been linked
+# this will fail on Windows
+if [ $HOME/.SpaceVim.d -ef `pwd`/.SpaceVim.d ]
+then
+  echo "[Succeeded]Already linked, do nothing."
+  exit 0
+fi
 
+wpath=`pwd`
+stamp=`date +%Y%m%d_%H%M%S`
+filename=".SpaceVim.d.$stamp.tar"
+
+echo ----------------
+echo "Backup .SpaceVim.d/ to $filename"
+echo Switch to HOME directory
+cd $HOME
+if tar -cvf $HOME/$filename .SpaceVim.d/
+then 
+  rm -rf $HOME/.SpaceVim.d
+else 
+  echo "Backup failed"
+fi
+
+echo Back to repo dir
+cd $wpath
+
+echo ----------------
+echo "Create soft link for .SpaceVim.d/"
+
+if !  ln -s `pwd`/.SpaceVim.d $HOME/.SpaceVim.d 
+then
+  echo "[Failed]Link folder failed."
+  exit 1
+else
+  echo "[Succeeded!]"
+fi
